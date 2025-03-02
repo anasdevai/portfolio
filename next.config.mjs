b76cd5f08d9b3d904c152ce2b3ed0ef32e6a3be6
MuhammadAnas02 <161252236+MuhammadAnas02@users.noreply.github.com>
2025-03-02 15:31:13 +0500

    ignoreDuring build

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,10 @@ const nextConfig = {
         domains: ['img.freepik.com', 'img.icons8.com', 'source.unsplash.com' , 'cdn.sanity.io'],
         
       },
+     eslint:{
+      ignoreDuringBuilds: true,
+      },
+
 };
 
 export default nextConfig;
