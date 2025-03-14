6fce3e4c738e81349b6b153bd59809847e3da63a
MuhammadAnas02 <161252236+MuhammadAnas02@users.noreply.github.com>
2025-03-15 04:31:05 +0500

    update the version

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['img.freepik.com', 'img.icons8.com', 'source.unsplash.com' , 'cdn.sanity.io'],
-        
+  images: {
+    domains: [
+      "img.freepik.com",
+      "img.icons8.com",
+      "source.unsplash.com",
+      "cdn.sanity.io"
+    ],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.icons8.com",
+        pathname: "/external-flaticons-lineal-color-flat-icons/**",
       },
-     eslint:{
-      ignoreDuringBuilds: true,
-      },
-
+    ],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
